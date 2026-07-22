399428b348192acf6256c0283b6e7586dfff7ea9
MnhNi <tuanminh802113@gmail.com>
2026-07-23 09:24:25 +1200

    Added working navigation buttons, Different frames for machines
    This is only the GUI, no Logic implemented yet, most things you see are placeholders and subject to change.
    Next version should have a better UI

--- 91906MinhNguyen.py
+++ 91906MinhNguyen.py
@@ -1,4 +1,3 @@
-"""THE SLOTMACHINE"""
 from tkinter import *
 
 
@@ -21,9 +20,9 @@ class GUI:
         self.frames = {}
         self.frames['Mainframe'] = self.create_main_frame()
         self.frames['Machine1'] = self.create_machine1_frame()
-        # self.frames['Machine2'] = self.create_machine2_frame()
-        # self.frames['Machine3'] = self.create_machine3_frame()
-        # self.frames['Machine4'] = self.create_machine4_frame()
+        self.frames['Machine2'] = self.create_machine2_frame()
+        self.frames['Machine3'] = self.create_machine3_frame()
+        self.frames['Machine4'] = self.create_machine4_frame()
 
 
         self.show_frame("Mainframe")
@@ -40,7 +39,7 @@ class GUI:
         
         Label(frame, font=FONT_MAIN_TITLE, text='Addiction').grid(row=0,columnspan=2,padx=10,pady=10,sticky='nsew')
 
-        Button(frame, text='PLAY', bg='white', font=FONT_HEADING, command=lambda:self.show_frame('Machine1')).grid(row=1,columnspan=2, padx=10, pady=10, sticky='nsew')
+        Button(frame, text='PLAY', bg='white', font=FONT_HEADING, command=lambda: self.show_frame('Machine1')).grid(row=1,columnspan=2, padx=10, pady=10, sticky='nsew')
 
         return frame
     
@@ -50,10 +49,95 @@ class GUI:
         frame.columnconfigure([0, 1, 2, 3, 4, 5, 6], minsize=50, weight=1)
         frame.rowconfigure([0, 1, 2, 3, 4, 5], minsize=50, weight=1)
 
-        Label(frame, font=FONT_DEFAULT, text='Action:').grid(row=0,column=1,padx=10,pady=10,sticky='nsew')
-        Label(frame, font=FONT_DEFAULT, text='5').grid(row=0,column=2,padx=10,pady=10,sticky='nsew')
+        #These are just placeholder Labels
+        Label(frame, font=FONT_DEFAULT, text='Day:').grid(row=0,column=5,padx=10,pady=10,sticky='nsew')
+        Label(frame, font=FONT_DEFAULT, text='2').grid(row=0,column=6,padx=10,pady=10,sticky='nsew')
+        
+        Label(frame, font=FONT_DEFAULT, text='Action:').grid(row=1,column=1,padx=10,pady=10,sticky='nsew')
+        Label(frame, font=FONT_DEFAULT, text='1').grid(row=1,column=2,padx=10,pady=10,sticky='nsw')
+
+        Label(frame, font=FONT_DEFAULT, text='Money:').grid(row=1,column=3,padx=10,pady=10,sticky='nsew')
+        Label(frame, font=FONT_DEFAULT, text='230').grid(row=1,column=4,padx=10,pady=10,sticky='nsw')
+
+        #Buttons
+        Button(frame, text='Menu', bg='white', font=FONT_HEADING, command=lambda: self.show_frame('Mainframe')).grid(row=0,column=0, padx=10, pady=10, sticky='nsew')
+        Button(frame, text='<', bg='white', font=FONT_HEADING, command=None).grid(row=1,column=0, padx=10, pady=10, sticky='nsew')
+        Button(frame, text='>', bg='white', font=FONT_HEADING, command=lambda: self.show_frame('Machine2')).grid(row=1,column=6, padx=10, pady=10, sticky='nsew')
 
 
+        return frame
+    
+    def create_machine2_frame(self):
+        frame = Frame(self.container)
+        frame.grid(row=0, column=0, sticky='nsew')
+        frame.columnconfigure([0, 1, 2, 3, 4, 5, 6], minsize=50, weight=1)
+        frame.rowconfigure([0, 1, 2, 3, 4, 5], minsize=50, weight=1)
+
+        #These are just placeholder Labels
+        Label(frame, font=FONT_DEFAULT, text='Day:').grid(row=0,column=5,padx=10,pady=10,sticky='nsew')
+        Label(frame, font=FONT_DEFAULT, text='2').grid(row=0,column=6,padx=10,pady=10,sticky='nsew')
+
+        Label(frame, font=FONT_DEFAULT, text='Action:').grid(row=1,column=1,padx=10,pady=10,sticky='nsew')
+        Label(frame, font=FONT_DEFAULT, text='2').grid(row=1,column=2,padx=10,pady=10,sticky='nsw')
+
+        Label(frame, font=FONT_DEFAULT, text='Money:').grid(row=1,column=3,padx=10,pady=10,sticky='nsew')
+        Label(frame, font=FONT_DEFAULT, text='230').grid(row=1,column=4,padx=10,pady=10,sticky='nsw')
+
+        #Buttons
+        Button(frame, text='Menu', bg='white', font=FONT_HEADING, command=lambda: self.show_frame('Mainframe')).grid(row=0,column=0, padx=10, pady=10, sticky='nsew')
+        Button(frame, text='<', bg='white', font=FONT_HEADING, command=lambda: self.show_frame('Machine1')).grid(row=1,column=0, padx=10, pady=10, sticky='nsew')
+        Button(frame, text='>', bg='white', font=FONT_HEADING, command=lambda: self.show_frame('Machine3')).grid(row=1,column=6, padx=10, pady=10, sticky='nsew')
+
+
+        return frame
+    
+    def create_machine3_frame(self):
+        frame = Frame(self.container)
+        frame.grid(row=0, column=0, sticky='nsew')
+        frame.columnconfigure([0, 1, 2, 3, 4, 5, 6], minsize=50, weight=1)
+        frame.rowconfigure([0, 1, 2, 3, 4, 5], minsize=50, weight=1)
+
+        #These are just placeholder Labels
+        Label(frame, font=FONT_DEFAULT, text='Day:').grid(row=0,column=5,padx=10,pady=10,sticky='nsew')
+        Label(frame, font=FONT_DEFAULT, text='2').grid(row=0,column=6,padx=10,pady=10,sticky='nsew')
+
+        Label(frame, font=FONT_DEFAULT, text='Action:').grid(row=1,column=1,padx=10,pady=10,sticky='nsew')
+        Label(frame, font=FONT_DEFAULT, text='3').grid(row=1,column=2,padx=10,pady=10,sticky='nsw')
+
+        Label(frame, font=FONT_DEFAULT, text='Money:').grid(row=1,column=3,padx=10,pady=10,sticky='nsew')
+        Label(frame, font=FONT_DEFAULT, text='230').grid(row=1,column=4,padx=10,pady=10,sticky='nsw')
+
+        #Buttons
+        Button(frame, text='Menu', bg='white', font=FONT_HEADING, command=lambda: self.show_frame('Mainframe')).grid(row=0,column=0, padx=10, pady=10, sticky='nsew')
+        Button(frame, text='<', bg='white', font=FONT_HEADING, command=lambda: self.show_frame('Machine2')).grid(row=1,column=0, padx=10, pady=10, sticky='nsew')
+        Button(frame, text='>', bg='white', font=FONT_HEADING, command=lambda: self.show_frame('Machine4')).grid(row=1,column=6, padx=10, pady=10, sticky='nsew')
+
+
+        return frame
+
+    def create_machine4_frame(self):
+        frame = Frame(self.container)
+        frame.grid(row=0, column=0, sticky='nsew')
+        frame.columnconfigure([0, 1, 2, 3, 4, 5, 6], minsize=50, weight=1)
+        frame.rowconfigure([0, 1, 2, 3, 4, 5], minsize=50, weight=1)
+
+        #These are just placeholder Labels
+        Label(frame, font=FONT_DEFAULT, text='Day:').grid(row=0,column=5,padx=10,pady=10,sticky='nsew')
+        Label(frame, font=FONT_DEFAULT, text='2').grid(row=0,column=6,padx=10,pady=10,sticky='nsew')
+
+        Label(frame, font=FONT_DEFAULT, text='Action:').grid(row=1,column=1,padx=10,pady=10,sticky='nsew')
+        Label(frame, font=FONT_DEFAULT, text='4').grid(row=1,column=2,padx=10,pady=10,sticky='nsw')
+
+        Label(frame, font=FONT_DEFAULT, text='Money:').grid(row=1,column=3,padx=10,pady=10,sticky='nsew')
+        Label(frame, font=FONT_DEFAULT, text='230').grid(row=1,column=4,padx=10,pady=10,sticky='nsw')
+
+        #Buttons
+        Button(frame, text='Menu', bg='white', font=FONT_HEADING, command=lambda: self.show_frame('Mainframe')).grid(row=0,column=0, padx=10, pady=10, sticky='nsew')
+        Button(frame, text='<', bg='white', font=FONT_HEADING, command=lambda: self.show_frame('Machine3')).grid(row=1,column=0, padx=10, pady=10, sticky='nsew')
+        Button(frame, text='>', bg='white', font=FONT_HEADING, command=None).grid(row=1,column=6, padx=10, pady=10, sticky='nsew')
+
+
+        return frame
 
 root = Tk()
 app = GUI(root)
